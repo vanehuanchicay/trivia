6874633c2bbd354f73034e3d31bca2ba83c20491
vanehuanchicay <vanesapardoh@gmail.com>
2018-02-17 09:29:46 -0300

    funciones respuestas

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,22 @@ fetch('https://opentdb.com/api.php?amount=10&category=11&difficulty=medium')
   })
 
   .then((data)=>{
-     console.log(data)
-  
+    console.log(data)
+    let category = data.results[0].category
+    let typeOp = data.results[0].type
+    let difficult = data.results[0].difficulty
+    let questionRe = data.results[0].question
+    let answerCorrect = data.results[0].correct_answer
+    let incorrectAnswers = data.results[0].incorrect_answers
+    
+
+    $('.info').append('<h5> Category: '+ category+ '</h5>' + 
+     '<h5> Difficulty: '+ difficult + '</h5>' + '<h3>' + questionRe +'</h3>' +
+     '<ul class="answers_wrong"><li>'+ answerCorrect + '</li></ul>');
+
+   for(let i = 0 ; i < incorrectAnswers.length; i ++){
+     $('.answers_wrong').append('<li>'+ incorrectAnswers[i]+ '</li>')
+    }
+   
 });
+

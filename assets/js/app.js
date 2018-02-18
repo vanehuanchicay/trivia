8fbc1015d0209be0f4b6e666bed5e775b4a27242
vanehuanchicay <vanesapardoh@gmail.com>
2018-02-18 16:59:58 -0300

    fixing counter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-fetch('https://opentdb.com/api.php?amount=10&category=11&difficulty=medium')
+fetch('https://opentdb.com/api.php?amount=15&category=11&difficulty=medium')
 .then((response)=>{
     console.log(response);
     return response.json();
@@ -6,9 +6,11 @@ fetch('https://opentdb.com/api.php?amount=10&category=11&difficulty=medium')
 
   .then((data)=>{
     console.log(data)
+    let counter = 0; 
     $('.enter_button').on('click', function(){
       $('.enter_button').hide(); // ocultar boton
-      let counter = 0; 
+      $('.info').empty(); // ocultar info 
+      
       let category = data.results[counter].category
       let typeOp = data.results[counter].type
       let difficult = data.results[counter].difficulty
@@ -24,71 +26,35 @@ fetch('https://opentdb.com/api.php?amount=10&category=11&difficulty=medium')
       console.log(allAnswers)
 
       $('.info').append(`<h5> Category: ${category} </h5> 
-        <h5> Difficulty: ${difficult} </h5> 
-        <h3>${question}</h3><div>
-        <div>
-        <button class="options">${allAnswers[counter,0]}</button>
-        </div>
-        <div>
-        <button class="options" >${allAnswers[counter,1]}</button><br>
-        </div>
-        <div>
-        <button class="options" >${allAnswers[counter, 2]}</button><br>
-        </div>
-        <div>
-        <button class="options" >${allAnswers[counter, 3]}</button><br>
-        </div>`)
+        <h5> Difficulty: ${difficult}</h5><h3>${question}</h3><div>
+        <div><button class="options">${allAnswers[0]}</button></div>
+        <div><button class="options" >${allAnswers[1]}</button></div>
+        <div><button class="options" >${allAnswers[2]}</button></div>
+        <div><button class="options" >${allAnswers[3]}</button></div>`)
 
       $('.options').on('click', function(){ // evento para respuestas
-        if($(this).text() === data.results[counter].correct_answer){
-          console.log('funciona')
+        if($(this).text() === data.results[counter].correct_answer  && counter !== 15){
           $('.info').empty(); // vaciar div contenedor info
-          $('.enter_button').text('Next Question'); // cambio contenido del boton
           $('.enter_button').show(); // vuelvo a mostrar mi boton
+          $('.enter_button').text('Next Question'); // cambio contenido del boton
           $('.info').append(`<h4>Correct answer!</h4>`);
+          
+        }counter ++; console.log(counter)
+        if($(this).text() !== data.results[counter].correct_answer  && counter !== 15){
+        $('.info').empty();
+        $('.enter_button').show();
+        $('.enter_button').text('Next Question');
+        $('.info').append(`<h4>Aww wrong answer!</h4><h5>The Correct Answer is
+          ${data.results[counter].correct_answer}</h5>`);
 
-            counter ++ 
-         
+        }counter ++;console.log(counter)
+
+        if(counter === 15){
+          console.log('juego terminado')
         }
-      
 
-      });
+      }); //end answers function
 
-
-
-      /*
-
-      $('.info').append(`<h5> Category: ${category} </h5> 
-       <h5> Difficulty: ${difficult} </h5> 
-       <h3>${question}</h3>
-
-       <button class="options">${allAnswers[counter]}</button><br>
-       <button class="options">${allAnswers[counter]}</button><br> 
-       <button class="options">${allAnswers[counter]}</button><br>
-       <button class="options">${allAnswers[counter]}</button>`)
-
-
-      $('.options').on('click',function(){ // evento para respuestas
-        if($(this).text() === data.results[counter].correct_answer){
-          console.log('funciona')
-          $('.enter_button').show(); // vuelvo a mostrar mi boton
-          $('.enter_button').text('Next Question'); // cambio contenido del boton
-          $('.info').empty(); // vaciar div contenedor info
-          $('.info').append(`<h4>Correct answer!</h4>`);
-
-        } counter ++ 
-
-        if($(this).text() !== data.results[counter].correct_answer){
-          $('.info').empty();
-          $('.enter_button').show();
-          $('.enter_button').text('Next Question');
-          $('.info').append(`<h4>Aww wrong answer!</h4><h5>The Correct Answer is
-            ${data.results[counter].correct_answer}</h5>`);
-
-        }counter ++;
-
-      })  // end evento onclick answers
-    */  
   }) //end enter button
   
 }); //end then data

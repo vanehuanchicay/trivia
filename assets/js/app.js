a77f6a9ed7e784713e1d9cef9e22f28346189caa
vanehuanchicay <vanesapardoh@gmail.com>
2018-02-19 16:52:43 -0300

    mistakes mistakes

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,27 +10,7 @@
   var provider = new firebase.auth.GoogleAuthProvider();
 
   function signIn(){
-    firebase.initializeApp(config);
-    var provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-}
-/*
   firebase.auth().signInWithRedirect(provider);
 
   firebase.auth().getRedirectResult().then(function(result) {
@@ -53,7 +33,7 @@
   });
   
 }
-*/
+
 
 fetch('https://opentdb.com/api.php?amount=15&category=11&difficulty=medium')
 .then((response)=>{
@@ -65,6 +45,12 @@ fetch('https://opentdb.com/api.php?amount=15&category=11&difficulty=medium')
     console.log(data)
     let correctAnswers = 0;
     let counter = 0; 
+    $('.title').removeClass('hidden');
+    $('.select_title').removeClass('hidden');
+    $('.multiple_option').removeClass('hidden');
+    $('.other_option').removeClass('hidden');
+    $('#google-sign').hide();
+  
     $('.multiple_option').on('click', function(){
       $('.select_title').hide();
       $('.other_option').hide(); // ocutar true/false selección

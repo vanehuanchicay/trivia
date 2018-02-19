910089ed8dfc3b5f71b961c2f8d998ccaa487658
vanehuanchicay <vanesapardoh@gmail.com>
2018-02-19 17:19:08 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,3 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCkeqW1Dx8h8Ov98dey7e0sa93-p_4P8_E",
-    authDomain: "trivia-8494a.firebaseapp.com",
-    databaseURL: "https://trivia-8494a.firebaseio.com",
-    projectId: "trivia-8494a",
-    storageBucket: "trivia-8494a.appspot.com",
-    messagingSenderId: "343758776919"
-  };
-  var provider = new firebase.auth.GoogleAuthProvider();
-
-  function signIn(){
-
-  firebase.auth().signInWithRedirect(provider);
-
-  firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-  }).catch(function(error){
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-
-}
-
 
 fetch('https://opentdb.com/api.php?amount=15&category=11&difficulty=medium')
 .then((response)=>{
